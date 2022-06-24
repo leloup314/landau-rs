11c7ae1f893444907e9d8e6ebf70a904c108fe13
leloup314 <wolf@physik.uni-bonn.de>
2022-06-25 01:07:02 +0200

    ENH: correct syntax in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+mod pdfs;
+
+
 #[cfg(test)]
 mod tests {
+    use super::pdfs::langau_pdf;
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+        let a = 15f64;
+        let b = 1f64;
+        let c = 1f64;
+        let d = 1f64;
+        assert_eq!(langau_pdf(a, b, c, d), 0.006297828887560015);
     }
 }
